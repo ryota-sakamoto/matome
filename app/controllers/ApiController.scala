24120bfcd02d7cf73740ffbaa86281fc2ea829e9
ryota.sakamoto <ryota.sakamoto@aijus.com>
2017-08-24 22:39:37 +0900

    add validate

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -5,22 +5,18 @@ import javax.inject._
 import models.Article
 import play.api.mvc._
 import play.api.libs.json.Json
+import utils.Validation
 
 @Singleton
 class ApiController @Inject() extends Controller {
     def getArticle = Action { implicit request =>
-        (for {
-            o <- request.getQueryString("offset")
-            l <- request.getQueryString("limit")
-        } yield (o.toInt, l.toInt) match {
-            case (offset, limit) => Article.find(offset, limit)
-            case _ => Article.find(0, 0)
-        }) match {
-            case Some(r) => {
-                val json = Json.toJson(r)
-                Ok(Json.stringify(json))
-            }
-            case None => BadRequest("bad request")
+        if (Validation.validate("offset" -> 'int, "limit" -> 'int)) {
+            val offset = request.getQueryString("offset").get.toInt
+            val limit = request.getQueryString("limit").get.toInt
+            val articles = Article.find(offset, limit)
+            Ok(Json.stringify(Json.toJson(articles)))
+        } else {
+            BadRequest("bad request")
         }
     }
 }
--- app/utils/Validation.scala
+++ app/utils/Validation.scala
@@ -1,25 +1,23 @@
 package utils
 
-object Validation {
-    private val number_only = """^(\d+)$""".r
-    private val url_regex = """https?://[a-zA-Z0-9]+?\.[a-zA-Z0-9/_:]+""".r
+import play.api.mvc.{AnyContent, Request}
 
-    def isNumber(t: Option[String]): Boolean = {
-        t match {
-            case Some(x) => {
-                x match {
-                    case number_only(n) => true
-                    case _ => false
-                }
+object Validation {
+    private val number_only = """^(\d+)$"""
+    private val url_regex = """https?://[a-zA-Z0-9]+?\.[a-zA-Z0-9/_:]+"""
+
+    def validate(args: (String, Symbol)*)(implicit request: Request[AnyContent]): Boolean = {
+        val m = for (
+            arg <- args
+        ) yield request.getQueryString(arg._1) match {
+            case Some(s) => arg._2 match {
+                case 'int => s.matches(number_only)
+                case 'url => s.matches(url_regex)
+                case _ => false
             }
             case None => false
         }
-    }
 
-    def isURL(url: String): Boolean = {
-        url match {
-            case url_regex() => true
-            case _ => false
-        }
+        !m.contains(false)
     }
 }
