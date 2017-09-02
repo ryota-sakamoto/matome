77587423279248034e76285671eb278a0d90356d
ryota.sakamoto <ryota.sakamoto@aijus.com>
2017-09-02 22:23:43 +0900

    add log

--- app/handlers/ErrorHandler.scala
+++ app/handlers/ErrorHandler.scala
@@ -3,19 +3,24 @@ package handlers
 import play.api.http.HttpErrorHandler
 import play.api.mvc._
 import play.api.mvc.Results._
+
 import scala.concurrent._
-import javax.inject.Singleton;
+import javax.inject.Singleton
+import play.api.Logger;
 
 @Singleton
 class ErrorHandler extends HttpErrorHandler {
+    val prefix = "[ErrorHandler]"
 
     def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
+        Logger.error(s"$prefix $message")
         Future.successful(
             NotFound(views.html.template.notfound())
         )
     }
 
     def onServerError(request: RequestHeader, exception: Throwable) = {
+        Logger.error(s"$prefix $exception")
         Future.successful(
             NotFound(views.html.template.notfound())
         )
