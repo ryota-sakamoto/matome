0e65ca7398dc337aea6ca754278957712e9473e0
ryota.sakamoto <ryota.sakamoto@aijus.com>
2017-09-03 20:44:54 +0900

    delete no use

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,7 +2,6 @@ package controllers
 
 import javax.inject._
 
-import models.Blog
 import utils.Security
 import play.api.mvc._
 import play.cache.CacheApi
@@ -13,8 +12,6 @@ class HomeController @Inject()(cache: CacheApi) extends Controller {
     def index = Action { implicit request =>
         val user_uuid = Security.getSessionUUID(request)
 
-        val blog = Blog.find()
-
-        Ok(views.html.home.index(cache, user_uuid, blog))
+        Ok(views.html.home.index(cache, user_uuid))
     }
 }
